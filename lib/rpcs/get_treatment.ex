fb77ed6aa7989e652219c986ffbb67b2df7a8af3
Ethan Gunderson <ethan@ethangunderson.com>
2024-06-20 14:11:57 -0500

    implement process caching

---
+++ lib/rpcs/get_treatment.ex
@@ -0,0 +1,36 @@
+defmodule Split.RPCs.GetTreatment do
+  alias Split.Treatment
+
+  @behaviour Split.RPC
+
+  @impl Split.RPC
+  @spec build(Keyword.t()) :: map()
+  def build(opts) do
+    user_key = Keyword.fetch!(opts, :user_key)
+    feature_name = Keyword.fetch!(opts, :feature_name)
+    bucketing_key = Keyword.get(opts, :bucketing_key, nil)
+    attributes = Keyword.get(opts, :attributes, %{})
+
+    %{
+      "v" => 1,
+      "o" => 0x11,
+      "a" => [
+        user_key,
+        bucketing_key,
+        feature_name,
+        attributes
+      ]
+    }
+  end
+
+  @impl Split.RPC
+  @spec parse_response(map(), Keyword.t()) :: {:ok, map()} | {:error, map()}
+  def parse_response(%{"s" => 1, "p" => treatment}, _) do
+    treatment = Treatment.build_from_daemon_response(treatment)
+    {:ok, treatment}
+  end
+
+  def parse_response(response, _) do
+    {:error, response}
+  end
+end
