fffe0cefeccdea7cf54921131987d09178f0b5c6
Dairon Medina Caro <dmedinacaro@cars.com>
2024-07-22 17:10:00 -0400

    Pool improvements
    * Better error handling for receive_message
    * Add a terminate_worker callback to close the socket when a worker is terminated
    * Add a Logger to the module
    * Make the socket connection async
    
    Co-authored-by: Pedro Rebollar <prebollar@cars.com>

--- lib/sockets/pool.ex
+++ lib/sockets/pool.ex
@@ -1,4 +1,6 @@
 defmodule Split.Sockets.Pool do
+  require Logger
+
   @behaviour NimblePool
 
   def child_spec(opts) do
@@ -11,16 +13,21 @@ defmodule Split.Sockets.Pool do
   def start_link(opts) do
     NimblePool.start_link(
       worker: {__MODULE__, opts},
+      # TODO: make this configurable
       # pool_size: pool_size,
-      lazy: false
+      lazy: true
     )
   end
 
   def send_message(message) do
-    NimblePool.checkout!(__MODULE__, :checkout, fn _caller, port ->
-      response = send_message(port, message)
-      {response, port}
-    end)
+    NimblePool.checkout!(
+      __MODULE__,
+      :checkout,
+      fn _caller, port ->
+        response = send_message(port, message)
+        {response, port}
+      end
+    )
   end
 
   def send_message(port, message) do
@@ -45,25 +52,37 @@ defmodule Split.Sockets.Pool do
 
   @impl NimblePool
   def init_worker(%{socket_path: socket_path} = pool_state) do
-    case :gen_tcp.connect({:local, socket_path}, 0,
-           reuseaddr: true,
-           active: false,
-           mode: :binary,
-           packet: 0
-         ) do
-      {:ok, port} ->
-        register_rpc = Split.RPC.Register.build()
+    parent = self()
 
-        :ok =
-          port
-          |> send_message(register_rpc)
-          |> Split.RPC.Register.parse_response()
+    connect_opts = [
+      :binary,
+      reuseaddr: true,
+      active: false,
+      packet: 0
+    ]
 
-        {:ok, port, pool_state}
+    # TODO: Make this configurable
+    connect_timeout = 1000
 
-      {:error, _reason} = error ->
-        {:ok, error, pool_state}
+    async = fn ->
+      case :gen_tcp.connect({:local, socket_path}, 0, connect_opts, connect_timeout) do
+        {:ok, socket} ->
+          :ok = :gen_tcp.controlling_process(socket, parent)
+
+          :ok =
+            socket
+            |> send_message(Split.RPC.Register.build())
+            |> Split.RPC.Register.parse_response()
+
+          socket
+
+        {:error, _reason} = error ->
+          Logger.error("Error establishing socket connection: #{inspect(error)}")
+          error
+      end
     end
+
+    {:async, async, pool_state}
   end
 
   @impl NimblePool
@@ -71,17 +90,29 @@ defmodule Split.Sockets.Pool do
     {:ok, worker_state, worker_state, pool_state}
   end
 
+  @impl NimblePool
+  def terminate_worker(reason, socket, pool_state) when is_port(socket) do
+    Logger.error("Terminating worker with reason: #{inspect(reason)}")
+    :gen_tcp.close(socket)
+    {:ok, pool_state}
+  end
+
+  def terminate_worker(_reason, _disconnected_socket, pool_state) do
+    {:ok, pool_state}
+  end
+
   defp receive_response({:error, reason}, _port) do
     {:error, reason}
   end
 
   defp receive_response(:ok, port) do
-    with {:ok, response} <- :gen_tcp.recv(port, 4) do
-      response_size = :binary.decode_unsigned(response, :little)
-
-      with {:ok, response} <- :gen_tcp.recv(port, response_size) do
-        Msgpax.unpack!(response)
-      end
+    with {:ok, <<response_size::little-unsigned-size(32)>>} <- :gen_tcp.recv(port, 4),
+         {:ok, response} <- :gen_tcp.recv(port, response_size) do
+      Msgpax.unpack!(response)
+    else
+      error ->
+        Logger.error("Error receiving response: #{inspect(error)}")
+        error
     end
   end
 end
