19b0f9db72af190a025311beeffaf6d5c2a437be
Dairon Medina Caro <dmedinacaro@cars.com>
2024-08-14 17:43:14 -0400

    Remove mimic

--- mix.exs
+++ mix.exs
@@ -29,8 +29,7 @@ defmodule SplitThinElixir.MixProject do
       # Dev and test dependencies
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
-      {:mimic, "~> 1.7", only: :test}
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 
