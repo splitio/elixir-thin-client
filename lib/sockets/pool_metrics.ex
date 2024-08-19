9179003625570fc6f9f08a227f63bec51a644163
Dairon Medina Caro <dmedinacaro@cars.com>
2024-08-19 15:32:35 -0400

    Implement pool utilization metrics

---
+++ lib/sockets/pool_metrics.ex
@@ -0,0 +1,65 @@
+defmodule Split.Sockets.PoolMetrics do
+  @moduledoc """
+  This module provides a way to store and update pool utilization metrics.
+  """
+  @type t :: %__MODULE__{}
+
+  defstruct [
+    :pool_size,
+    :connections_available,
+    :connections_in_use
+  ]
+
+  @metrics [
+    :pool_size,
+    :connections_in_use
+  ]
+
+  def init(pool_name, pool_size) do
+    ref = :atomics.new(length(@metrics), signed: false)
+    :atomics.add(ref, metric_index(:pool_size), pool_size)
+
+    :persistent_term.put({__MODULE__, :metrics, pool_name}, ref)
+    {:ok, ref}
+  end
+
+  @spec update(:atomics.atomics_ref(), {atom(), integer()}) :: :ok
+  def update(ref, {metric, val}) when not is_nil(ref) and metric in @metrics do
+    :atomics.add(ref, metric_index(metric), val)
+  end
+
+  def update(_ref, _metric), do: :ok
+
+  @spec get(module()) :: {:ok, t()} | {:error, :not_found}
+  def get(pool_name) do
+    case :persistent_term.get({__MODULE__, :metrics, pool_name}, nil) do
+      nil -> {:error, :not_found}
+      ref -> get_metrics(ref)
+    end
+  end
+
+  defp get_metrics(ref) do
+    %{
+      pool_size: pool_size,
+      connections_in_use: connections_in_use
+    } =
+      @metrics
+      |> Enum.with_index()
+      |> Enum.map(fn {metric, i} -> {metric, :atomics.get(ref, i + 1)} end)
+      |> Map.new()
+
+    {:ok,
+     %__MODULE__{
+       pool_size: pool_size,
+       connections_available: pool_size - connections_in_use,
+       connections_in_use: connections_in_use
+     }}
+  end
+
+  defp metric_index(metric) do
+    case Enum.find_index(@metrics, &(&1 == metric)) do
+      nil -> raise ArgumentError, "Invalid metric: #{inspect(metric)}"
+      index -> index + 1
+    end
+  end
+end
