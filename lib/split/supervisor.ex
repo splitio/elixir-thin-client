02ad93cc1f476d8dcd3ef8f181e459f4be83cf85
Emiliano Sanchez <emiliano.sanchez@split.io>
2025-02-14 13:02:40 -0300

    Update start_link function

--- lib/split/supervisor.ex
+++ lib/split/supervisor.ex
@@ -11,8 +11,9 @@ defmodule Split.Supervisor do
     {:ok, init_arg}
   end
 
+  @spec start_link() :: Supervisor.on_start()
   @spec start_link(Split.options()) :: Supervisor.on_start()
-  def start_link(opts) do
+  def start_link(opts \\ []) do
     child = {Pool, opts}
     Supervisor.start_link([child], strategy: :one_for_one)
   end
