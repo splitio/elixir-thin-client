7f67983c2a53d9a5a67c77bd49e4daf538309d31
Emiliano Sanchez <emiliano.sanchez@split.io>
2025-12-17 12:16:29 -0300

    Stop treating 'socket closed at check-in time' as a log-worthy error event and only remove the worker when the pool explicitly gets :closed

--- lib/split/sockets/pool.ex
+++ lib/split/sockets/pool.ex
@@ -139,13 +139,16 @@ defmodule Split.Sockets.Pool do
   def handle_checkin(checkin, _from, _old_conn, {_opts, metrics_ref} = pool_state) do
     PoolMetrics.update(metrics_ref, {:connections_in_use, -1})
 
-    with {:ok, conn} <- checkin,
-         true <- Conn.is_open?(conn) do
-      {:ok, conn, pool_state}
-    else
-      _ ->
+    case checkin do
+      {:ok, conn} ->
+        {:ok, conn, pool_state}
+
+      :closed ->
+        {:remove, :closed, pool_state}
+
+      other ->
         Logger.debug(
-          "Error checking in socket #{inspect(checkin)} to the pool. Socket is closed."
+          "Error checking in socket #{inspect(other)} to the pool. Socket is closed."
         )
 
         {:remove, :closed, pool_state}
